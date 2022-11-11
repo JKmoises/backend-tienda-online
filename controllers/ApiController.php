69956dc7e046b715e436ee761c958252e587c730
JKmoises <moi.prado@duocuc.cl>
2022-11-11 13:25:48 -0300

    Agregando headers para peticion a la api

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -16,6 +16,8 @@ class ApiController{
 
     $productosPorCategoria = ProductoCategoria::SQL($query);
 
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json");
     echo json_encode($productosPorCategoria);
   }
 
