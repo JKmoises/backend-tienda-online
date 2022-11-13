274a73947c057143a83f78f416ea3c7603c55fcc
JKmoises <moi.prado@duocuc.cl>
2022-11-13 10:49:58 -0300

    Revisión final

--- Router.php
+++ Router.php
@@ -19,7 +19,7 @@ class Router{
     
     $url_actual = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
     $method = $_SERVER['REQUEST_METHOD'];
-
+    
     if ($method === 'GET') {
       $fn = $this->getRoutes[$url_actual] ?? null;
     } else {
